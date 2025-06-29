e35caa3905c3855a4ff19a96d4dc2995e982b498
unknown <divakurniawan2003@gmail.com>
2025-06-29 23:59:26 +0700

    Aku commit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,16 +5,16 @@ use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:api')->group(function () {
+    Route::get('/index2', [TestController::class, 'index2']);
+});
+
 Route::group([
     'prefix' => 'auth'
 ], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::middleware('auth:api')->group(function () {
-        Route::get('/index2', [TestController::class, 'index2']);
-    });
 
     Route::group(['middleware' => 'admin'], function () {
         Route::get('/index', [TestController::class, 'index']);
