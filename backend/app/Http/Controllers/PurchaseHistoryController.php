7273db327bc0d2e9c2ab5c8794aeec7aa6ff641e
T3DJE <gamingkoper667@gmail.com>
2025-07-01 02:36:47 +0700

    Add new order, purchasehistory, and orderitem table

---
+++ backend/app/Http/Controllers/PurchaseHistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseHistory;
+use Illuminate\Http\Request;
+
+class PurchaseHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PurchaseHistory $purchaseHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PurchaseHistory $purchaseHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PurchaseHistory $purchaseHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PurchaseHistory $purchaseHistory)
+    {
+        //
+    }
+}
